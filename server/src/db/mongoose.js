c717dd999e8eda0414112c3c4a58833ed745999d
Sunny Khanuja <khanujasunny91@gmail.com>
2021-10-26 23:23:31 +0530

    deleted server

--- server/src/db/mongoose.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const conn = mongoose.connect('mongodb://127.0.0.1:27017/elections',{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
